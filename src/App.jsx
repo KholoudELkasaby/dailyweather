e42450c7201f09ffb31f1187ca19ca515eaef47e
Kholoud <141324962+Ssypha@users.noreply.github.com>
2024-07-21 22:38:28 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import Temperature from "./componenets/Temperature";
+import Highlights from "./componenets/Highlights";
+import useLocalStorage from "use-local-storage"
+
+function App() {
+  const preference = window.matchMedia("(prefers-color-scheme:light)").matches;
+  const [lightMode,setLightMode] = useLocalStorage("lightMode",preference);
+  const toggleLightMode = () => {
+    setLightMode(!lightMode);
+  }
+
+  const [city, setCity] = useState("Ismailia");
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=7740193bdeb645b1a1344122240104&q=${city}&aqi=no;`;
+
+    fetch(apiUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not get data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [city]);
+  return (
+    <div className={`${lightMode && "dark"}`}>
+    <div className="bg-slate-800 h-screen flex justify-center  items-start dark:bg-blue-200">
+      <div className="w-1/5 h-1/3 mt-40">
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              isDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime,
+            }}
+          />
+        )}
+      </div>
+      <div className="w-1/3 h-1/3 mt-40 p-10 grid grid-cols-2 gap-6">
+        <h1 className="text-slate-200 text-2xl col-span-2 dark:text-black">
+          Today's Forecast
+        </h1>
+        {weatherData && (
+          <>
+            <Highlights 
+              
+              stats={{
+                title: "Wind Status",
+                value: weatherData.current.wind_mph,
+                unit: "mph",
+                direction: weatherData.current.wind_dir,
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Humidity",
+                value: weatherData.current.humidity,
+                unit: "%",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Visibility",
+                value: weatherData.current.vis_miles,
+                unit: "miles",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Air Pressure",
+                value: weatherData.current.pressure_mb,
+                unit: "mb",
+              }}
+            />
+          </>
+        )}
+      </div>
+      <button onClick={toggleLightMode} className="absolute w-8 h-8 top-16 left-72 bg-blue-600 rounded-3xl text-black font-semibold dark:bg-blue-500 dark:text-white">
+        {lightMode? <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white ml-1">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+         </svg></div>:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24  24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-1 ">
+       <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+      </svg>}
+      </button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
